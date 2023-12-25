056c0187a01b9d970464c077a051e5383416c3f9
ibrahimmedhat <doha.kroz0@gmail.com>
2023-12-25 18:55:58 +0300

    Initial commit

---
+++ lib/layout/manager/layout_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'layout_state.dart';
+
+class LayoutCubit extends Cubit<LayoutState> {
+  LayoutCubit() : super(LayoutInitial());
+  static LayoutCubit get(context) => BlocProvider.of(context);
+}
